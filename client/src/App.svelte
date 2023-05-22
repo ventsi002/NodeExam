4cf9293fc66c87ead5ece79796fc674ee408c1af
Daniel <dannysht27@gmail.com>
2023-05-22 13:17:15 +0200

    App.svelte changed to how it is supposed to

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,39 +4,3 @@
 </script>
 
 <Navigation/>
-
-<style>
-    .nav {
-        background-color: #2f3e46;
-        font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-        line-height: 1.5;
-        font-weight: 500;
-        font-size: 21px;
-        padding: 21px;
-        min-width: 97.5vw;
-        position: relative;
-        left: -12px;
-        top: -8px;
-        text-decoration: none;
-        display: flex;
-        justify-content: space-between;
-        color: #cce3de;
-    }
-
-    p {
-        display: inline;
-    }
-    p:hover {
-        color: #a4c3b2;
-    }
-
-    svg:hover {
-        color: #a4c3b2;
-    }
-
-    svg {
-        width: 35px;
-        height: 35px;
-        cursor: pointer;
-    }
-</style>
